aefefd0215a26acdabb0fd96472c2255f33e9d28
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-01-26 20:22:05 +0100

    Commit with not scale constrains

--- lessonOneExerciseTwo/ViewController.swift
+++ lessonOneExerciseTwo/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButtonLabel: UIButton!
+    
+    var isButtonPressed = false
+    var colorToTurn = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.height / 2
     }
 
-
+    @IBAction func startButtonPressed() {
+        if !isButtonPressed {
+            startButtonLabel.setTitle("NEXT", for: .normal)
+            isButtonPressed.toggle()
+        }
+        
+        switch colorToTurn {
+        case 1:
+            redLightView.alpha = 1.0
+            greenLightView.alpha = 0.3
+            colorToTurn = 2
+        case 2:
+            yellowLightView.alpha = 1.0
+            redLightView.alpha = 0.3
+            colorToTurn = 3
+        default:
+            greenLightView.alpha = 1.0
+            yellowLightView.alpha = 0.3
+            colorToTurn = 1
+        }
+    }
+    
 }
 
